7a8379f1702c56e915e263f3e48affe11f3a6966
wakandacurry <edgtg0304@gmail.com>
2019-11-08 20:58:29 +0300

    186 Improving Aiming
    Shrotest turns of Barrel

--- Source/BattleTank/Private/TankPlayerController.cpp
+++ Source/BattleTank/Private/TankPlayerController.cpp
@@ -27,11 +27,15 @@ void ATankPlayerController::AimTowardsCrosshair()
 { 
 	if (!GetPawn())
 		return;
+
 	auto AimingComponent = GetPawn()->FindComponentByClass<UTankAimingComponent>();
 	if (!ensure(AimingComponent))
 		return;
+
 	FVector HitLocation; // Out Parameter
-	if (GetSightRayHitLocation(HitLocation))
+	bool bGotHitLocation = GetSightRayHitLocation(HitLocation);
+
+	if (bGotHitLocation)
 	{
 		AimingComponent->AimAt(HitLocation);
 	}
@@ -50,10 +54,10 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector & HitLocation) const
 	FVector LookDirection;
 	if (GetLookDirection(ScreenLocation, LookDirection))
 	{
-		GetLookVectorHitLocation(LookDirection, HitLocation);
+		return GetLookVectorHitLocation(LookDirection, HitLocation);
 	}
 
-	return true;
+	return false;
 }
 
 
